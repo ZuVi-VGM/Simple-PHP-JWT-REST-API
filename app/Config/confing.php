b3e8534d1efb2c524c5d7cad6e13767a36727ddb
ZuVi-VGM <vitog.m98@gmail.com>
2024-02-08 16:35:29 +0100

    Generated config with secret key for the api

---
+++ app/Config/confing.php
@@ -0,0 +1,12 @@
+<?php
+
+/*****************************
+ * General const
+ *****************************/
+ const DEV_MODE = true; //Define if in development
+
+ /****************************
+  * Encryption data
+  ****************************/
+ const SECRET_KEY = '8b3e3d0e67745f713bca9bce3e44411108f922cf5e58efb60866d4d2678042e0'; //Key used in hmac encryption
+
