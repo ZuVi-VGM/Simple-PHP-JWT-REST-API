be6ff39d58a25174be88b1d24f65eb2d519d5862
ZuVi-VGM <vitog.m98@gmail.com>
2024-02-08 18:02:56 +0100

    Updated index to handle an example request

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
+require_once __DIR__.'/app/includes/autoload.php';
 
-echo 'Hello world!';
+use App\Controllers\RestController;
+use App\Routing\Router;
+
+$restController = new RestController();
+$router = new Router($restController);
+
+$router->addRoute('GET','/getdata', 'testMethod');
+
+$method = $_SERVER['REQUEST_METHOD'];
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+// Instrada la richiesta al router
+$router->addRoute('GET', '/Simple-PHP-JWT-REST-API/test', 'test');
+$router->routeRequest($method, $path);
