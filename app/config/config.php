6b659560e4c0148ee1e5f4869270e4d26b8b4b64
ZuVi-VGM <vitog.m98@gmail.com>
2024-02-08 17:52:59 +0100

    Updated configuration file

---
+++ app/config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+/*****************************
+ * General const
+ *****************************/
+ const DEV_MODE = true; //Define if in development
+ const APP_PATH = __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR; //DO NOT CHANGE THIS SETTING!
+ 
+ /****************************
+  * Encryption data
+  ****************************/
+ const SECRET_KEY = '8b3e3d0e67745f713bca9bce3e44411108f922cf5e58efb60866d4d2678042e0'; //Key used in hmac encryption
+
+
