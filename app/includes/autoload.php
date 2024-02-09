caa3caa3a0d99b31627d666868495d04390dce92
ZuVi-VGM <vitog.m98@gmail.com>
2024-02-09 17:27:12 +0100

    updated autoload function

--- app/includes/autoload.php
+++ app/includes/autoload.php
@@ -2,17 +2,16 @@
 require_once __DIR__.'/config/config.php';
 spl_autoload_register(function ($className) {
     $classMap = array(
-        'App\\Controllers\\' => 'Controllers/',
-        'App\\Models\\' => 'Models/',
-        'App\\Routing\\' => 'Routing/',
-        'App\\Utils\\' => 'Utils/'
+        'App\\Controllers\\' => 'Controllers',
+        'App\\Models\\' => 'Models',
+        'App\\Routing\\' => 'Routing',
+        'App\\Utils\\' => 'Utils'
     );
 
     // Cerca la classe nel classMap
     foreach ($classMap as $namespace => $directory) {
         if (strpos($className, $namespace) === 0) {
-            $directory = str_replace('/', DIRECTORY_SEPARATOR, $directory);
-            $filePath = str_replace($namespace, $directory, $className) . '.php';
+            $filePath = str_replace($namespace, $directory.DIRECTORY_SEPARATOR, $className) . '.php';
             $fullPath = APP_PATH . $filePath;
 
             if (file_exists($fullPath)) {
