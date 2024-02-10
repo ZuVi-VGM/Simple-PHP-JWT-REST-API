4f7f16d0c1c8bb88b70bf718f486bbbf1cbf8604
ZuVi-VGM <vitog.m98@gmail.com>
2024-02-10 13:10:27 +0100

    Completed router class and fixed small problems

--- app/Routing/Router.php
+++ app/Routing/Router.php
@@ -2,30 +2,44 @@
 
 namespace App\Routing;
 use App\Models\ApiResponse;
+use App\Controllers\RestController;
 
 class Router {
-    private $routes = [];
+    private $routes = ['GET' => [], 'POST' => [], 'PUT' => [], 'DELETE' => []];
     private $restController;
     private $apiResponse;
 
-    public function __construct($rest_controller) {
+    public function __construct(RestController $rest_controller) {
         $this->restController = $rest_controller;
+        $this->apiResponse = new ApiResponse;
     }
 
-    public function addRoute($method, $path, $action) {
-        $this->routes[$method][$path] = $action;
+    public function addRoute(string $method, string $path, string $action) {
+        $method = strtoupper($method);
+        if($this->checkMethod($method))
+            $this->routes[$method][$path] = $action;
+        else
+            throw new \Exception("Method not supported. Must be one of GET, POST, PUT or DELETE");
+
+        var_dump($this->routes);
+    }
+
+    private function checkMethod($method){
+        return array_key_exists($method, $this->routes);
     }
 
     public function routeRequest($method, $path) {
         if (isset($this->routes[$method][$path])) {
-            //$action = $this->routes[$method][$path];
-            //call_user_func_array([$this->restController, $action], []);
-            //header('Content-Type: application/json');
-
+            $action = $this->routes[$method][$path];
+            header('Content-Type: application/json');
+            $resdata = call_user_func_array([$this->restController, $action], []);
+            $this->apiResponse->prepareResponse(200, 'OK', $resdata);
+            echo $this->apiResponse->toJSON();
         } else {
             // Gestione dell'errore 404
             http_response_code(404);
-            $this->apiResponse = new ApiResponse('404', '404 Not Found', ['error' => true, 'code' => 404]);
+            header('Content-Type: application/json');
+            $this->apiResponse->prepareResponse(404, 'Not Found', ['error' => 404, 'message' => '404 Not Found']);
             echo $this->apiResponse->toJson();
         }
     }
